83a7c12ef139b6aa40b9936ba6bbf75b5fea3928
Daniel <danielbyomujuni@gmail.com>
2024-03-05 03:00:53 -0500

    added grouping system

--- include/c_test.h
+++ include/c_test.h
@@ -1,8 +1,10 @@
 #include <stdio.h>
 #include <string.h>
+#include <stdlib.h>
 
 #define EXIT_FAILURE -1
-#define STATUS_LENGTH 6
+#define STATUS_LENGTH 7
+#define MAX_GROUP_LENGTH 80
 //public macros
 
 /**
@@ -78,13 +80,13 @@ __attribute__((used, section("c_test"))) const test_struct* const dummy = NULL;
 
 static void test_passed(const char* testName) {
     int padlen = ( STATUS_LENGTH - strlen("Passed")) / 2;
-    printf("\033[0;32m[ %*s%s%*s ]", padlen, "", "Passed", padlen, "");
+    printf("  \033[0;32m[ %*s%s%*s ]", padlen, "", "Passed", padlen, "");
     printf("\033[0;37m %s\n", testName);
 }
 
 static void test_failed(const char* testName) {
     int padlen = ( STATUS_LENGTH - strlen("Failed")) / 2;
-    printf("\033[0;31m[ %*s%s%*s ]", padlen, "", "Failed", padlen, "");
+    printf("  \033[0;31m[ %*s%s%*s ]", padlen, "", "Failed", padlen, "");
     printf("\033[0;37m %s\n", testName);
 }
 
@@ -93,18 +95,55 @@ static int run_tests() {
     //get tests
     //sort them into groups
     //run the tests
-    int did_test_fail = 0;
+    test_struct **tests = calloc(0, sizeof(struct test_struct *));
+    int test_len = 0;
+    //create tests
     for (const test_struct * const* it = TEST_DATA_BEGIN; it != TEST_DATA_END; it++) {
         if (*it == NULL) {
             continue;
         }
-        test_struct test = **it;
-        test.run(&test);
+        //
+        test_struct *temp_arr[test_len];
+        for (int i = 0; i < test_len; i++) {
+            temp_arr[i] = tests[i];
+        }
+        free(tests);
+        tests = calloc(test_len,sizeof(struct test_struct *));
+        for (int i = 0; i < test_len; i++) {
+            tests[i] = temp_arr[i];
+        }
+        test_len++;
+        test_struct temp_test = **it;
 
-        if (test.did_test_pass != 1) {
+        test_struct *new_test = calloc(1, sizeof(test_struct));
+        *new_test = temp_test;
+        tests[test_len - 1] = new_test;
+
+    }
+
+    for(int i=0; i<test_len; i++){
+        for(int j=i+1; j<test_len; j++){
+            if(strcmp(tests[i]->group_name,tests[j]->group_name)>0){
+                test_struct *temp = tests[i];
+                tests[i] = tests[j];
+                tests[j] = temp;
+            }
+        }
+    }
+
+    int did_test_fail = 0;
+    char current_group[MAX_GROUP_LENGTH];
+    for (int i = 0; i < test_len; i++) {
+        if (strcmp(current_group, tests[i]->group_name) != 0) {
+            strcpy(current_group, tests[i]->group_name);
+            printf("\033[0;1m\033[0;36m[-GROUP-]\033[0;37m %s \n",current_group);
+        }
+        tests[i]->run(tests[i]);
+        if (tests[i]->did_test_pass != 1) {
             did_test_fail = EXIT_FAILURE;
         }
     }
+
     return did_test_fail;
 }
 
--- main.c
+++ main.c
@@ -20,3 +20,20 @@ TEST(HelloWorld, EXPECT_TRUE) {
 TEST(HelloWorld, EXPECT_FALSE) {
     ASSERT_FALSE(0);
 }
+
+TEST(GoodbyeWorld, EXPECT_EQUAL) {
+    ASSERT_EQ(1,1);
+}
+
+TEST(GoodbyeWorld, EXPECT_NOT_EQUAL) {
+    ASSERT_NOT_EQ(1,2);
+}
+
+TEST(GoodbyeWorld, EXPECT_TRUE) {
+    ASSERT_TRUE(1);
+}
+
+//fails memory error
+TEST(GoodbyeWorld, EXPECT_FALSE) {
+    ASSERT_FALSE(0);
+}
